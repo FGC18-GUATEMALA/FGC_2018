3945da95ed269760a730462a087eac25e91e0471
Unknown <gabriel.monzon39@gmail.com>
2018-06-24 23:44:07 -0600

    Primer Test Motor
    El motor va a moverse lento cuando "A" sea presionado

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MotorTest1_SlowWhenButtonPressed.java
@@ -0,0 +1,35 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+
+@TeleOp (name = "MotorTest1", group = "Tests")
+public class MotorTest1_SlowWhenButtonPressed extends LinearOpMode {
+
+    private DcMotor motor1 = null;
+
+    @Override
+    public void runOpMode(){
+
+        motor1 = hardwareMap.get(DcMotor.class, "motor1");
+        waitForStart();
+
+        while (opModeIsActive()){
+
+            double speed1 = 0, speed2 = 0;
+            boolean buttonA = false;
+
+            speed1 = gamepad1.left_stick_y;
+            buttonA = gamepad1.a;
+
+            if (buttonA == true){
+                speed2 = speed1 * 0.3;
+            }else{
+                speed2 = speed1;
+            }
+
+            motor1.setPower(speed2);
+        }
+    }
+}
